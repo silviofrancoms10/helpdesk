2502e40c2b0764abdd0da1ecce0a0dc6014dff12
silviofrancoms10 <silviofrancoms@gmail.com>
2024-09-19 18:16:46 -0400

    Instanciando alguns exemplos para rodar no inicio da aplicação

--- src/main/java/com/silviofrancoms/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/silviofrancoms/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,46 @@
 package com.silviofrancoms.helpdesk;
 
+import com.silviofrancoms.helpdesk.domain.Chamado;
+import com.silviofrancoms.helpdesk.domain.Cliente;
+import com.silviofrancoms.helpdesk.domain.Tecnico;
+import com.silviofrancoms.helpdesk.domain.enums.Perfil;
+import com.silviofrancoms.helpdesk.domain.enums.Prioridade;
+import com.silviofrancoms.helpdesk.domain.enums.Status;
+import com.silviofrancoms.helpdesk.repositories.ChamadoRepository;
+import com.silviofrancoms.helpdesk.repositories.ClienteRepository;
+import com.silviofrancoms.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
     }
 
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Tecnico tec1 = new Tecnico(null, "Silvio Franco", "12345678900", "silvio@mail.com.br", "123");
+        tec1.addPerfil(Perfil.ADMIN);
+
+        Cliente cli1 = new Cliente(null, "Linus Torvalds", "12345678901", "torvalds@mail.com", "123");
+
+        Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        chamadoRepository.saveAll(Arrays.asList(c1));
+    }
 }
