1052f6d5ea147938aef16cf706c8071fa1bba8f5
silviofrancoms10 <silviofrancoms@gmail.com>
2024-09-20 06:10:14 -0400

    Instanciação de exemplos no perfil de test

---
+++ src/main/java/com/silviofrancoms/helpdesk/config/TestConfig.java
@@ -0,0 +1,21 @@
+package com.silviofrancoms.helpdesk.config;
+
+import com.silviofrancoms.helpdesk.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public DBService instanciaDB() {
+        this.dbService.instanciaDB();
+        return null;
+    }
+}
