551f021713e819be242194b98bd5e88929d09f51
silviofranco <silviofrancoms@gmail.com>
2024-09-29 19:44:58 -0400

    Protegendo aplicação de serialização ciclica

--- src/main/java/com/silviofrancoms/helpdesk/domain/Cliente.java
+++ src/main/java/com/silviofrancoms/helpdesk/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.silviofrancoms.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.silviofrancoms.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 public class Cliente extends Pessoa {
     private static final long serialVersionUID = 1L;
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
--- src/main/java/com/silviofrancoms/helpdesk/domain/Tecnico.java
+++ src/main/java/com/silviofrancoms/helpdesk/domain/Tecnico.java
@@ -1,5 +1,6 @@
 package com.silviofrancoms.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.silviofrancoms.helpdesk.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 public class Tecnico extends Pessoa {
     private static final long serialVersionUID = 1L;
+    @JsonIgnore
     @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
