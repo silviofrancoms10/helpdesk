cea2bd6a8d62a1a7c815334a5914d0b6e3bc89c1
silviofranco <silviofrancoms@gmail.com>
2025-02-19 02:36:32 -0400

    Padrão DTO para Tecnico

---
+++ src/main/java/com/silviofrancoms/helpdesk/domain/TecnicoDTO.java
@@ -0,0 +1,95 @@
+package com.silviofrancoms.helpdesk.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.silviofrancoms.helpdesk.domain.enums.Perfil;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TecnicoDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+    protected String name;
+    protected String cpf;
+    protected String email;
+    protected String password;
+    protected Set<Integer> perfis = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public TecnicoDTO() {
+    }
+
+    public TecnicoDTO(Tecnico obj) {
+        this.id = obj.getId();
+        this.name = obj.getName();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.password = obj.getPassword();
+        this.perfis = obj.getPerfis().stream().map(Perfil::getCode).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(Perfil::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfis.add(perfil.getCode());
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
