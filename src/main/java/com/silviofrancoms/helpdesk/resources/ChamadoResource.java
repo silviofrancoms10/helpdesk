8828c1fc5da3286091aa2590a6f6d8d45e67593d
silviofranco <silviofrancoms@gmail.com>
2025-02-25 22:13:05 -0400

    implementado update chamado

--- src/main/java/com/silviofrancoms/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/silviofrancoms/helpdesk/resources/ChamadoResource.java
@@ -39,4 +39,9 @@ public class ChamadoResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> update(@PathVariable Integer id, @Valid @RequestBody ChamadoDTO objDTO) {
+        Chamado newObj = chamadoService.update(id, objDTO);
+        return ResponseEntity.ok().body(new ChamadoDTO(newObj));
+    }
 }
--- src/main/java/com/silviofrancoms/helpdesk/services/ChamadoService.java
+++ src/main/java/com/silviofrancoms/helpdesk/services/ChamadoService.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,7 +37,13 @@ public class ChamadoService {
 
     public Chamado create(@Valid ChamadoDTO objDTO) {
         return chamadoRepository.save(newChamado(objDTO));
+    }
 
+    public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+        objDTO.setId(id);
+        Chamado oldObj = findById(id);
+        oldObj = newChamado(objDTO);
+        return chamadoRepository.save(oldObj);
     }
 
     private Chamado newChamado(ChamadoDTO objDTO) {
@@ -46,6 +53,10 @@ public class ChamadoService {
         Chamado chamado = new Chamado();
         if (objDTO.getId() != null){
             chamado.setId(objDTO.getId());
+        }
+
+        if (objDTO.getStatus().equals(Status.ENCERRADO.getCode())) {
+            chamado.setDataFechamento(LocalDate.now());
         }
 
         chamado.setTecnico(tecnico);
